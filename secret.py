f43e4e646abddd8bf02bc72a1560822ae011201b
Keaton <linux.keaton@gmail.com>
2018-06-04 07:48:13 -0500

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,11 @@
+## Reddit
+R_USER = "" #username
+R_PASS = "" #password
+R_CLID = "" #client ID
+R_SCRT = "" #secret
+
+## Imgur
+I_CLID = "" #client ID
+I_SCRT = "" #secret
+I_AXSS = "" #access token
+I_REFR = "" #refresh token
